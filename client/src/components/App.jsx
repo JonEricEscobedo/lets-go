918cd9705b3a0f65160804b0d97fcb028e5392a4
Jon Eric Escobedo <jonericescobedo@gmail.com>
2017-08-29 13:23:18 -0700

    (feat) add search box and button

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,13 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Search from './Search.jsx';
 import Dashboard from './Dashboard.jsx';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      query: ''
     }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.submitQuery = this.submitQuery.bind(this);
+    this.fetchPlaces = this.fetchPlaces.bind(this);
+  }
+
+  // Detects input in text box
+  handleChange(event) {
+    this.setState( {query: event.target.value} );
+  }
+
+  // Submits query
+  submitQuery() {
+    this.fetchPlaces();
+    this.setState( {query: ''} );
+  }
+
+  // Google Places External API
+  fetchPlaces() {
+    console.log(this.state.query);
   }
 
   render() {
@@ -20,7 +41,11 @@ class App extends React.Component {
         </header>
 
         <nav>
-          <Search />
+          <Search 
+            query={this.state.query}
+            handleChange={this.handleChange}
+            submitQuery={this.submitQuery}
+          />
         </nav>
 
         <section>
--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -4,22 +4,26 @@ import ReactDOM from 'react-dom';
 class Search extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      
-    }
   }
+
+
 
   render() {
     return (
       <div>
+
         <div className="row">
           <div className="col-sm-6">
             <div className="md-form">
-                <input type="text" id="location-search" className="form-control" />
+                <input type="text" id="location-search" className="form-control" value={this.props.query} onChange={this.props.handleChange} />
                 <label htmlFor="location-search" className="">Where do you want to go?</label>
             </div>
           </div>
+          <div className="col-sm-2">
+            <button type="button" className="btn btn-default" onClick={this.props.submitQuery}>Find!</button>
+          </div>
         </div>
+
       </div>
     );
   }
